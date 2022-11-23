ffe601b3fcd7a70f204f31910ac01b0cdab62c9d
Gabriel-Aco <gabriel.santos@acovisa.com.br>
2022-11-23 16:43:56 -0300

    Alterando Rotas do App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ export default function App() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<Navigate to="/" />}/>
 
           {/* Pagina inicial */}
           <Route path="/administracao" element={<Link to={'/administracao/cadastroUsuario'}> Adm ir para cadastro </Link>}/>
@@ -12,7 +11,7 @@ export default function App() {
           {/* Rotas administração */}
           <Route path="/administracao/cadastroUsuario" element={<Link to={'/administracao/listaUsuarios'}>Cadastro ir para Lista</Link>}/>
           <Route path="/administracao/listaUsuarios" element={<Link to={'/administracao/editaUsuario'}>Lista ir para Edita</Link>}/>
-          <Route path="/administracao/editaUsuario/" element={<Link to={'/administracao'}>Edita ir para Adm</Link>}/>
+          <Route path="/administracao/editaUsuario/" element={<Link to={'/'}>Edita ir para Home</Link>}/>
 
         </Routes>
       </BrowserRouter>
